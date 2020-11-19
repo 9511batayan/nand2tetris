33e253ee8aead7e2f1ab9c6e54fd728108e84133
Koji <hosinken446@gmail.com>
2020-11-19 19:56:32 +0900

    04 complete

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,56 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    //Keyboard input
+    @keyboard
+    D=M
+
+    //Jump to ON if pressed
+    @ON
+    D;JGT
+
+    //If no pressed, set R0 to 0(0(0b0000000000000000)
+    @R0
+    M=0
+
+    //Jump to FILL
+    @FILL
+    0;JMP
+
+(ON)
+    //Set R0 to -1(0b1111111111111111)
+    @R0
+    M=-1
+
+(FILL)
+    //Initialize the drawing start position (lower right of the screen) and go to R1
+    @SCREEN
+    D=A
+    @8191   //8192 = (512 / 16) * 256 - 1
+    D=D+A
+    @R1
+    M=0
+
+    (FILLLOOP)
+        //Value to draw
+        @R0
+        D=M
+
+        //Draw on R1
+        @R1
+        A=M //Set the drawing position address in the A register
+        M=0
+
+        //Decrement the address of the drawing position
+        @R1
+        MD=M-1
+
+        //Jump to FILLLOOP until you reach the top left of the screen
+        @SCREEN
+        D=D-A
+        @FILLLOOP
+        D;JGE
+
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,21 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R1
+    D=M
+    @R2
+    M=0     // R2 = 0 <-- initialize
+    @END
+    D;JLE   // If R1<=0 Then Jump to (END)
+(LOOP)
+    @R0
+    D=M
+    @R2
+    M=M+D   // R2 = R2 + R0
+    @R1
+    D=D-1   // R1 = R1 - 1
+    @LOOP
+    0;JLT   // If R1>0 Then Jump to (LOOP)
+(END)
+    @END
+    0;JMP   // Infinite loop terminates Hack program.
